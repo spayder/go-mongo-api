5e5ac36a118f518e31f9d4f13c387f443d93a9dd
Piotr <gawronski.p@gmail.com>
2024-03-09 12:14:48 +0100

    Initial commit with project setup

--- db/db.go
+++ db/db.go
@@ -1,1 +1,31 @@
 package db
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	username := os.Getenv("MONGO_DB_USERNAME")
+	password := os.Getenv("MONGO_DB_PASSWORD")
+
+	clientOptions.SetAuth(options.Credential{
+		Username: username,
+		Password: password,
+	})
+
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	log.Println("Connected to mongo")
+
+	return client, nil
+}
