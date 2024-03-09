55e9f32ab5de70093461e79b3280cc4049f773f5
Piotr <gawronski.p@gmail.com>
2024-03-09 16:12:20 +0100

    Wip

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+type Application struct {
+}
+
 func main() {
 	fmt.Println("It works")
 }
--- services/todo.go
+++ services/todo.go
@@ -1,1 +1,23 @@
 package services
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Todo struct {
+	ID        string    `json:"id,omitempty" bson:"_id,omitempty"`
+	Task      string    `json:"task,omitempty" bson:"task,omitempty"`
+	Completed bool      `json:"completed,omitempty" bson:"completed,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+}
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Todo {
+	client = mongo
+
+	return Todo{}
+}
