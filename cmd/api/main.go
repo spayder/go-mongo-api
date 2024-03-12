a0790e83030382d193933d90a11778f22cfd81bd
Piotr <gawronski.p@gmail.com>
2024-03-12 23:21:06 +0100

    Work in progress

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/spayder/go-mongo-api/db"
+	"github.com/spayder/go-mongo-api/handlers"
+	"github.com/spayder/go-mongo-api/services"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type Application struct {
+	models services.Models
 }
 
 func main() {
-	fmt.Println("It works")
+	mongoClient, err := db.ConnectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+
+	defer cancel()
+
+	defer func() {
+		if err = mongoClient.Disconnect(ctx); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	services.New(mongoClient)
+
+	log.Fatal(http.ListenAndServe(":8080", handlers.CreateRouter()))
 }
---
+++ handlers/router.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+func CreateRouter() chi.Mux {
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTION"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CRSF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	router.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
